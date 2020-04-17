586aaab28e97300f983809b299e7164576ab6168
Mr.Pink <56068154+ool3@users.noreply.github.com>
2020-04-17 16:43:40 +0300

    Some update simple game
    Error: When ball disappear in the left part of the screen or on the top everything is good, but when ball disappear on the bottom or on the right errors come up!

--- Tinker/Game_ball.py
+++ Tinker/Game_ball.py
@@ -1,4 +1,6 @@
 import tkinter
+
+
 def key_pressed(event):
     if event.keysym == 'space':
         canvas.coords(oval, (300, 300, 310, 310))
@@ -14,6 +16,22 @@ def key_pressed(event):
     if canvas.coords(oval)[1] < 200 or canvas.coords(oval)[1] > 400 or canvas.coords(oval)[0] < 200 or \
             canvas.coords(oval)[0] > 400:
         canvas.itemconfig(oval, fill='red')
+    if int(canvas.coords(oval)[1]) == 600 or int(canvas.coords(oval)[0]) == 600 or \
+            int(canvas.coords(oval)[1]) == 0 or int(canvas.coords(oval)[0]) == 0:
+        move_wrap(canvas, oval, canvas.coords(oval))
+
+
+def move_wrap(canvas, obj, move):
+    if int(move[0]) == 0:
+        canvas.move(obj, 590, 0)
+    elif int(move[0]) == 600:
+        canvas.move(obj, 0, 00)
+    elif int(move[1]) == 0:
+        canvas.move(obj, 0, 590)
+    elif move[-1] == 600.0:
+        canvas.move(obj, 0, 0)
+
+
 master = tkinter.Tk()
 canvas = tkinter.Canvas(master, bg='blue', height=600, width=600)
 oval = canvas.create_oval((300, 300), (310, 310), fill='green')
